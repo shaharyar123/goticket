021f56984c47ffcb1ee4c42bf8b207ee3f51930e
Muhammad-Saqib <saqibrizwan7400@gmail.com>
2017-06-17 17:49:12 +0500

    process complete

--- modules/home/controllers.js
+++ modules/home/controllers.js
@@ -5,5 +5,17 @@ angular.module('Home')
 .controller('HomeController',
     ['$scope',
     function ($scope) {
+
+        $scope.allFiles = [
+            {title: 'my doc', description: 'Donec eu libero sit amet quam egestas semper. Aenean ultricies mi ' +
+            'vitae est. Mauris placerat eleifend leo Donec eu libero sit amet quam egestas semper. Aenean ultricies ' +
+            'mi vitae est. Mauris placerat eleifend leo', submitionDate: '12/12/12', fileName: 'purposalFYP.docx'},
+            {title: 'my doc', description: 'Donec eu libero sit amet quam egestas semper. Aenean ultricies mi ' +
+            'vitae est. Mauris placerat eleifend leo Donec eu libero sit amet quam egestas semper. Aenean ultricies ' +
+            'mi vitae est. Mauris placerat eleifend leo', submitionDate: '12/12/12', fileName: 'purposalFYP.docx'},
+            {title: 'my doc', description: 'Donec eu libero sit amet quam egestas semper. Aenean ultricies mi ' +
+            'vitae est. Mauris placerat eleifend leo Donec eu libero sit amet quam egestas semper. Aenean ultricies ' +
+            'mi vitae est. Mauris placerat eleifend leo', submitionDate: '12/12/12', fileName: 'abcx.docx'}
+        ]
       
     }]);
