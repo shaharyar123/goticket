ff227853d19d2a69a0ca69824e9d8a3ea9031c34
shairy <shairy333@gmail.com>
2017-06-25 17:11:52 +0500

    updated

--- modules/home/controllers.js
+++ modules/home/controllers.js
@@ -7,15 +7,15 @@ angular.module('Home')
     function ($scope) {
 
         $scope.allFiles = [
-            {title: 'my doc', description: 'Donec eu libero sit amet quam egestas semper. Aenean ultricies mi ' +
-            'vitae est. Mauris placerat eleifend leo Donec eu libero sit amet quam egestas semper. Aenean ultricies ' +
-            'mi vitae est. Mauris placerat eleifend leo', submitionDate: '12/12/12', fileName: 'purposalFYP.docx'},
-            {title: 'my doc', description: 'Donec eu libero sit amet quam egestas semper. Aenean ultricies mi ' +
-            'vitae est. Mauris placerat eleifend leo Donec eu libero sit amet quam egestas semper. Aenean ultricies ' +
-            'mi vitae est. Mauris placerat eleifend leo', submitionDate: '12/12/12', fileName: 'purposalFYP.docx'},
-            {title: 'my doc', description: 'Donec eu libero sit amet quam egestas semper. Aenean ultricies mi ' +
-            'vitae est. Mauris placerat eleifend leo Donec eu libero sit amet quam egestas semper. Aenean ultricies ' +
-            'mi vitae est. Mauris placerat eleifend leo', submitionDate: '12/12/12', fileName: 'abcx.docx'}
+            {title: 'Proposal', description: 'This file contains the proposal of the project submitted to FYPCED cabin at the beginning', submitionDate: '07/02/17', fileName: 'proposal.docx'},
+            {title: 'Objects | Features', description: 'This file contains the Objects and features of the project', submitionDate: '07/04/17', fileName: 'objectAndFeatures.docx'},
+            {title: 'SRS Report', description: 'This file contains the SRS(software requirements specification) Report of the project', submitionDate: '17/04/17', fileName: 'sRs.docx'},
+            {title: 'Risk Factors', description: 'This file contains the Risk Factors Documentation of the project', submitionDate: '17/04/17', fileName: 'riskFactor.docx'},
+            {title: 'ERD', description: 'This file contains the ERD(Entity Relation Diagram) of the project', submitionDate: '02/05/17', fileName: 'erd.docx'},
+            {title: 'Flow Chart', description: 'This file contains the Flow Chart (algorithm, workflow or process) of the project', submitionDate: '02/05/17', fileName: 'flow.docx'},
+            {title: 'Gantt Chart', description: 'This file contains the Complete Gantt Chart of the project', submitionDate: '02/05/17', fileName: 'gantt.docx'},
+            {title: 'System Diagram', description: 'This file contains the Complete System Diagram of the project', submitionDate: '15/05/17', fileName: 'systemDiagram.docx'},
+            {title: 'Use-Case Diagram', description: 'This file contains the Complete Use Case Diagram of the project', submitionDate: '15/05/17', fileName: 'useCase.docx'}
         ]
       
     }]);
